5b0c4c5ef5d5abec5d78adf39f3602aa43155309
Pierretoms <pierre.toms13@gmail.com>
2025-04-01 16:21:04 +0200

    fix de la connection login

--- my-react-app/src/actions/login.action.js
+++ my-react-app/src/actions/login.action.js
@@ -1,4 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
 
 export const login = createAsyncThunk("login", async ({ email, password, remember }) => {
   const response = await axios.post("http://localhost:3001/api/v1/user/login", {
